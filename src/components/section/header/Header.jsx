0f927f5aba0282ad1498a2acb448e57526975df2
mark-ua-top <mark.d.nahorniak@gmail.com>
2026-02-25 13:18:33 +0200

    fix

--- src/components/section/header/Header.jsx
+++ src/components/section/header/Header.jsx
@@ -10,16 +10,37 @@ export const Header = ({ onAuthClick, user, logout }) => {
         <section className="header">
             <div className="container header-container">
                 <img className="Header-logo" src={logo} alt="Logo" />
+
                 <ul className="Header-list">
                     <li><button className="Header-page-button">Who we are</button></li>
                     <li><button className="Header-page-button">Contacts</button></li>
                     <li><button className="Header-page-button">Menu</button></li>
                 </ul>
+
                 {user
-                    ? <button className="Header-sign-up-button" onClick={() => { logout(); localStorage.removeItem('authToken'); }}>Logout</button>
-                    : <button className="Header-sign-up-button" onClick={onAuthClick}>Sign Up</button>
+                    ? (
+                        <>
+                            <button
+                                className="Header-sign-up-button"
+                                onClick={() => {
+                                    logout();
+                                    localStorage.removeItem('authToken');
+                                }}
+                            >
+                                Logout
+                            </button>
+                            <img className="Header-user-image" src={avatar} alt="user-avatar" />
+                        </>
+                    )
+                    : (
+                        <button
+                            className="Header-sign-up-button"
+                            onClick={onAuthClick} // викликає App.js для відкриття SignUp/SignIn модалки
+                        >
+                            Sign Up
+                        </button>
+                    )
                 }
-                <img className="Header-user-image" src={avatar} alt="user-image" />
             </div>
         </section>
     );
